8786f1d5311850260dc24f14786fd1f6dec627b3
Rohan Yadav <ryy3490@gmail.com>
2023-09-27 14:35:41 +0530

    sm sshema

--- app/routes/stock_man.py
+++ app/routes/stock_man.py
@@ -12,21 +12,38 @@ router = APIRouter(prefix='/smanager', tags=["Stock Manager"])
             status_code=status.HTTP_200_OK)
 def get_requisition_requests(db: Session = Depends(get_db)):
 
-    req = db.query(models.Requisition).all()
+    req_query = db.query(models.Requisition, models.Material).join(models.Material, models.Material.id == models.Requisition.m_id).all()
+
+    request = [
+        {
+            "req_id" : req.req_id,
+            "req_by" : req.req_by,
+            "qty" : req.qty_req,
+            "remarks" : req.remarks,
+            "req_at" : req.req_time,
+            "material" : {
+                "id": mat.id,
+                "name": mat.material,
+                "group": mat.g_no,
+                "umo": mat.umo,
+            },
+        }
+        for req, mat in req_query
+    ]
 
     return {
             "status" : "200",
-            "data" : req
+            "data" : request
         }
 
-@router.get("/requisition" ,
-            #  response_model=tSchemas.RequisitionOut,
-            status_code=status.HTTP_200_OK)
-def get_requisition_requests(emp_id: int, db: Session = Depends(get_db)):
+# @router.get("/requisition" ,
+#             #  response_model=tSchemas.RequisitionOut,
+#             status_code=status.HTTP_200_OK)
+# def get_requisition_requests(emp_id: int, db: Session = Depends(get_db)):
 
-    req = db.query(models.Requisition).filter(models.Requisition.req_by == emp_id).all()
+#     # filter(models.Requisition.req_by == emp_id)
 
-    return {
-            "status" : "200",
-            "data" : req
-        }
+#     return {
+#             "status" : "200",
+#             "data" : req
+#         }
